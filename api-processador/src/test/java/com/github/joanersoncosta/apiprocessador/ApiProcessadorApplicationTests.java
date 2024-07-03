b1b558ef91fab698b39b4906d28e466fbbe1a98a
Joanerson Costa <joanersonc31@gmail.com>
2024-07-03 12:02:58 -0300

    inicia api procesador

---
+++ api-processador/src/test/java/com/github/joanersoncosta/apiprocessador/ApiProcessadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.apiprocessador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProcessadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
