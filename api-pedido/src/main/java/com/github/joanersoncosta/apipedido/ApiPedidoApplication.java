2ff687e710aca6b92b4a39257169cf5ba7f2c75d
Joanerson Costa <joanersonc31@gmail.com>
2024-07-02 21:39:43 -0300

    inicia projeto

---
+++ api-pedido/src/main/java/com/github/joanersoncosta/apipedido/ApiPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.apipedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidoApplication.class, args);
+	}
+
+}
---
+++ api-pedido/src/test/java/com/github/joanersoncosta/apipedido/ApiPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.apipedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
