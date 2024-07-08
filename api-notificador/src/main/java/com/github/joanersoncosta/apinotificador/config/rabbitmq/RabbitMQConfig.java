dc85e2bf09ba866d1a3d2c0c1eb00f53101189e5
Joanerson Costa <joanersonc31@gmail.com>
2024-07-08 16:46:50 -0300

    configura rabbit config

---
+++ api-notificador/src/main/java/com/github/joanersoncosta/apinotificador/config/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,72 @@
+package com.github.joanersoncosta.apinotificador.config.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class RabbitMQConfig {
+
+	@Value("${rabbit.exchange.name}")
+	private String exchangeName;
+	@Value("${rabbit.queue.name}")
+	private String queueName;
+	
+	@Bean
+	public FanoutExchange pedidosExchange() {
+		return new FanoutExchange(exchangeName);
+	}
+	
+	@Bean
+	public Queue queue() {
+		return new Queue(queueName);
+	}
+	
+	@Bean
+	public Binding binding() {
+		return BindingBuilder.bind(queue()).to(pedidosExchange());
+	}
+	
+	@Bean
+	public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+		return new RabbitAdmin(connectionFactory);
+	}
+	
+	@Bean
+	public MessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(messageConverter);
+		return rabbitTemplate;
+	}
+	
+	@Bean
+	public ApplicationListener<ApplicationReadyEvent> applicationReadyEventListener(RabbitAdmin rabbitAdmin){
+		return event -> rabbitAdmin.initialize();
+	}
+	
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
